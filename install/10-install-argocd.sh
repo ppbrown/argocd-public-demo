a8a2818ecfa021d9d2abf96ec90e637486bc681e
Philip Brown <phil@bolthole.com>
2025-11-04 15:52:15 -0800

    Update rollout status check for Argo CD installation
    Replaced argocd-application-controller rollout status check with argocd-applicationset-controller.

--- install/10-install-argocd.sh
+++ install/10-install-argocd.sh
@@ -7,7 +7,7 @@ kubectl get ns "$NS" >/dev/null 2>&1 || kubectl create ns "$NS"
 # Official multi-tenant install manifest
 kubectl apply -n "$NS" -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# Wait for the API/UI to be up (not strictly required to sync apps, but nice)
+# These DO nothing. They just give pretty output while you wait for install to complete
 kubectl -n "$NS" rollout status deploy/argocd-server --timeout=180s
 kubectl -n "$NS" rollout status deploy/argocd-repo-server --timeout=180s
-kubectl -n "$NS" rollout status deploy/argocd-application-controller --timeout=180s
+kubectl -n "$NS" rollout status deploy/argocd-applicationset-controller --timeout=180s
