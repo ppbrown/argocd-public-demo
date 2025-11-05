a92399adbda8dc0f9a323da8cc71c494466854cd
Philip Brown <phil@bolthole.com>
2025-11-04 21:05:09 -0800

    Fix namespace variable for ArgoCD rollout status

--- install/10-install-argocd.sh
+++ install/10-install-argocd.sh
@@ -2,6 +2,6 @@ kubectl create ns argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # These DO nothing. They just give pretty output while you wait for install to complete
-kubectl -n "$NS" rollout status deploy/argocd-server --timeout=180s
-kubectl -n "$NS" rollout status deploy/argocd-repo-server --timeout=180s
-kubectl -n "$NS" rollout status deploy/argocd-applicationset-controller --timeout=180s
+kubectl -n argocd rollout status deploy/argocd-server --timeout=180s
+kubectl -n argocd rollout status deploy/argocd-repo-server --timeout=180s
+kubectl -n argocd rollout status deploy/argocd-applicationset-controller --timeout=180s
