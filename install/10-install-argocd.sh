37e189b2d34aa48e21769e965e34e9c1e0f76746
Philip Brown <phil@bolthole.com>
2025-11-04 21:02:11 -0800

    Simplify ArgoCD installation script

--- install/10-install-argocd.sh
+++ install/10-install-argocd.sh
@@ -1,11 +1,5 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-NS=argocd
-kubectl get ns "$NS" >/dev/null 2>&1 || kubectl create ns "$NS"
-
-# Official multi-tenant install manifest
-kubectl apply -n "$NS" -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create ns argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # These DO nothing. They just give pretty output while you wait for install to complete
 kubectl -n "$NS" rollout status deploy/argocd-server --timeout=180s
